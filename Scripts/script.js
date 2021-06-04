00680084998283731c2ecfc022d69c7f3c0c4daa
Priyadarshan Sarkar <33196122+mrinmoyf2@users.noreply.github.com>
2021-06-04 15:06:01 +0530

    main page change

---
+++ Scripts/script.js
@@ -0,0 +1,12 @@
+  
+    function myfun() {
+        var a = document.getElementById("password").Value;
+
+        if (a == "lunaahan") {
+            console.log("hi");
+            document.getElementById("hide-text").style.display = " ";
+
+        }
+            
+        
+    }
